4429ef69b824e7ed343d2d038030d2757c404977
Wasley Carvalho <88601440+wasleyfps@users.noreply.github.com>
2024-04-17 19:51:30 -0300

    add: mini project generator passwords

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <GeneratorPassword/>
+    <footer><p>Projeto desenvolvido por <a href="https://www.wasley.com.br/">Wasley Carvalho</a>  ❤️☕</p></footer>
+  </div>  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GeneratorPassword from './components/GeneratorPassword.vue';
+
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GeneratorPassword
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  footer {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  padding: 10px;
+  background-color: #f5f5f5;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  border-top: 1px solid #ccc;
+  color: #333;
 }
 </style>
---
+++ src/components/GeneratorPassword.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="password-generator-container">
+    <h2 class="password-generator-title">Gerador de Senhas</h2>
+    <label for="length">Tamanho da Senha</label>
+    <input type="number" id="length" v-model="passwordLength" min="4" max="32">
+    <br>
+
+    <label for="includeUppercase">Incluir Letras Maiúsculas: </label>
+    <input type="checkbox" id="includeUppercase" v-model="includeUppercase">
+    <br>
+    
+    <label for="includeNumbers">Incluir Números: </label>
+    <input type="checkbox" id="includeNumbers" v-model="includeNumbers">
+    <br>
+    
+    <label for="includeSymbols">Incluir Símbolos: </label>
+    <input type="checkbox" id="includeSymbols" v-model="includeSymbols">
+    <br>
+    <button @click="generatePassword" class="generatedPassword-button">Gerar Senha</button>
+
+    <div v-if="generatePassword" class="generated-password">
+      <strong>Sua Senha: </strong> {{  generatedPassword }}
+    </div>
+  </div>
+
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const passwordLength = ref(12);
+const includeUppercase = ref(true);
+const includeNumbers = ref(true);
+const includeSymbols = ref(true);
+const generatedPassword = ref("");
+
+const generatePassword = () => {
+  const lowercase = "abcdefghijklmnopqrstuvwxyz";
+  const uppercase = includeUppercase.value ? "ABCDEFGHIJKLMNOPQRSTUVWXYZ" : "";
+  const numbers = includeNumbers.value ? "0123456789" : "";
+  const symbols = includeNumbers.value ? "!@#$%^&*()_+[]{}|;:,.<>?/~`" : "";
+
+  let password = "";
+
+  const all = lowercase + uppercase + numbers + symbols;
+  for (let i = 0; i < passwordLength.value; i++){
+    const randomIndex = Math.floor(Math.random() * all.length);
+    password += all[randomIndex];
+  }
+
+  generatedPassword.value = password;
+};
+</script>
+
+<style scoped>
+
+.password-generator-container{
+  max-width: 400px;
+  margin: 50px auto;
+}
+.password-generator-title{
+  font-size: 24px;
+  color: #333;
+  margin-bottom: 20px;
+
+}
+
+input {
+  width: 100%;
+  padding: 8px;
+  margin-bottom: 10px;
+}
+
+.generate-button {
+  padding: 10px 15px;
+  font-size: 16px;
+  background-color: #3498db;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+
+.generatedPassword-button{
+  background-color: #949494;
+  border-radius: 4px;
+}
+
+.generatedPassword-button:hover{
+  background-color: #c2c2c2;
+}
+
+
+.generated-password{
+  margin-top: 20px;
+  padding: 15px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  background-color: #f9f9f9;
+  color: #333;
+}
+</style>
